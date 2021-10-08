8f98e24a7c7d7dac5b0a5104de818d3672dc2f99
eredotpkfr <erdoganyoksul3@gmail.com>
2021-10-08 11:29:28 +0300

    fix: clang-format fixes in types.h header file

--- include/types.h
+++ include/types.h
@@ -2,10 +2,10 @@
  * Type definitions are listed in this header file.
  */
 
-#include <stdbool.h> /* for bool data types */
-
 #ifndef _TYPES_H_
 #define _TYPES_H_
+
+#include <stdbool.h> /* for bool data types */
 
 // Struct type definition for store splitted results
 typedef struct split_t {
